9924bd9f0115e4681e50dc25b7ed68257c11c7df
john3245 <jonathan.jabb61@outlook.com>
2022-04-07 21:43:07 -0500

    5

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const STATIC_CACHE = 'static-v1';
 const DYNAMIC_CACHE = 'dynamic-v1';
 const INMUTABLE_CACHE = 'inmutable-v1';
 const APP_SHELL = [
-  "./","index.html","css/style.css","img/favico.ico","js/app.js","img/avs/blackbull.jpg","img/avs/cat.png","img/avs/kangoroo.png","pages/offline.html","js/sw-acces.js"
+  "./","index.html","css/style.css","js/app.js","img/avs/blackbull.jpg","img/avs/cat.png","img/avs/kangaroo.png","img/icons/icon_app.png","pages/offline.html","js/sw-acces.js"
 ];
 const APP_SHELL_INMUTABLE = [
   "pages/offline.html","https://fonts.googleapis.com/css?family=Lato:400,300","https://fonts.googleapis.com/css?family=Quicksand:300,400","https://netdna.bootstrapcdn.com/font-awesome/3.1.1/css/font-awesome.css","js/libs/jquery.js"
